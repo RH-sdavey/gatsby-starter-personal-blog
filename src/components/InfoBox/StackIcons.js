66291664d02808cb2b7af990406a56cae71d6697
Sean Davey <32485509+RH-sdavey@users.noreply.github.com>
2019-08-03 22:29:28 +0200

    fixup stack 3

--- src/components/InfoBox/StackIcons.js
+++ src/components/InfoBox/StackIcons.js
@@ -44,7 +44,7 @@ const styles = theme => ({
     fontSize: ".85em",
     letterSpacing: ".3em",
     width: "100%",
-    margin: "2em 0 .8em 0",
+    margin: "2em 0 .4em 2em",
     fontWeight: 300
   }
 });
